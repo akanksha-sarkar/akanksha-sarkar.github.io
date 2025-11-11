5834c6c2ae5c2b0771f2d17c1573b0cc56c758d9
akanksha <as2637@cornell.edu>
2025-11-10 22:06:43 -0500

    sorry i am a little slow and take a while to get things right..should be fixed now

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://akkij26.github.io',
+  site: 'https://akanksha-sarkar.github.io/',
   integrations: [mdx(), sitemap(), tailwind()],
   base: '/'
 });
